28a114c49a7d18d85ceb213767142ed071c0eaf5
Immolazione <federico.deered@gmail.com>
2022-03-17 17:27:03 +0100

    print movies with details from api to page

--- public/js/app.js
+++ public/js/app.js
@@ -19414,10 +19414,10 @@ window.axios.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest';
 
 /***/ }),
 
-/***/ "./resources/sass/app.scss":
-/*!*********************************!*\
-  !*** ./resources/sass/app.scss ***!
-  \*********************************/
+/***/ "./resources/sass/home.scss":
+/*!**********************************!*\
+  !*** ./resources/sass/home.scss ***!
+  \**********************************/
 /*! no static exports found */
 /***/ (function(module, exports) {
 
@@ -19426,14 +19426,14 @@ window.axios.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest';
 /***/ }),
 
 /***/ 0:
-/*!*************************************************************!*\
-  !*** multi ./resources/js/app.js ./resources/sass/app.scss ***!
-  \*************************************************************/
+/*!**************************************************************!*\
+  !*** multi ./resources/js/app.js ./resources/sass/home.scss ***!
+  \**************************************************************/
 /*! no static exports found */
 /***/ (function(module, exports, __webpack_require__) {
 
 __webpack_require__(/*! C:\Users\feder\Desktop\laravel-model-controller\resources\js\app.js */"./resources/js/app.js");
-module.exports = __webpack_require__(/*! C:\Users\feder\Desktop\laravel-model-controller\resources\sass\app.scss */"./resources/sass/app.scss");
+module.exports = __webpack_require__(/*! C:\Users\feder\Desktop\laravel-model-controller\resources\sass\home.scss */"./resources/sass/home.scss");
 
 
 /***/ })
--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -4,11 +4,21 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="{{asset('css/home.css')}}">
     <title>Model Controller</title>
 </head>
 <body>
-    @foreach($movies as $movie)
-    <h2>{{ $movie->title }}</h2>
-    @endforeach
+    <div class="container">
+        <div class="card">
+            @foreach($movies as $movie)
+            <ul>
+                <li><h1>{{ $movie->title }}</h1></li>
+                <li>{{ $movie->original_title }}</li>
+                <li>{{ $movie->nationality }}</li>
+                <li>{{ $movie->vote }}</li>
+            </ul>
+            @endforeach
+        </div>
+    </div>
 </body>
 </html>
